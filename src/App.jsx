45bbde9c3dcfd0f25d01d6950485340c6f2faf22
Rafael da Costa Silva <rafael1501.oficial@gmail.com>
2025-06-05 15:15:43 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { SectionProvider } from "./SectionContext.jsx";
-import Main from "./components/1-home/Home.jsx";
+import Home from "./components/1-home/Home.jsx";
 import Search from "./components/5-search/Search.jsx";
 import Catalog from "./components/2-catalog/Catalog.jsx";
 import Modal from "./components/3-modal/Modal.jsx";
@@ -17,7 +17,7 @@ function AppContent() {
 
   return (
     <>
-      <Main />
+      <Home />
       <Search />
       <Catalog category="Ordem Paranormal" />
       <Catalog category="Ordem Paranormal" />
